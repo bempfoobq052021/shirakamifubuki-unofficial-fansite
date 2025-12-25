97ac6ed03d60793922d2db67ea7057a1becbe7d8
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2025-12-25 07:28:15 +0700

    One single comma in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 		paths: {
 			// @see https://svelte.dev/docs/kit/adapter-static#GitHub-Pages
 			// I added base here and in deploy and it doesn't work. Why.
-			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
 		}
 	},
 	extensions: ['.svelte', '.svx']
