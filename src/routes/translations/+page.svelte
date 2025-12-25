6c28c7e20e42b53a33011013fa807d2044109f38
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2025-12-25 20:48:20 +0700

    Changing the translation page to not use slug

--- src/routes/translations/+page.svelte
+++ src/routes/translations/+page.svelte
@@ -1,20 +1,33 @@
 <script>
-    let { data } = $props();
+    import { posts } from './tld.js';
+    let count = $state(0);
+    let post = $derived(posts[count]);
+    function backclick(){
+        if(count < 0) count = 0;
+        else count = count - 1;
+    }
+    function frontclick(){
+        if(count < 0) count = 0;
+        else count = count + 1;
+    }
 </script>
 
 <div class="overflow-x-hidden bg-image-paper bg-cover p-10">
-    <div class="w-<80%>">
-        <div class="w-<50%> flex justify-start text-black">
-            <a href="/translations/{ data.post.prev }">&larr;{ data.post.prev }</a>
+    <div class="w-<80%> flex justify-between">
+        <div class="text-black">
+            <a onclick={backclick} href="">&larr;{ post.prev }</a>
         </div>
-        <div class="w-<50%> flex justify-end text-black">
-            <a href="/translations/{ data.post.next }">{ data.post.next }&rarr;</a>
+        <div class="text-black">
+            <a href="./index.html">Home</a>
+        </div>
+        <div class="text-black">
+            <a onclick={frontclick} href="">{ post.next }&rarr;</a>
         </div>
     </div>
     <div class="w-full text-fbksite-darkblue ">
         <p class="text-[2rem] md:text-[4rem] text-center 
         fbkingdom-novel-translation font-weight-400">
-            { data.post.title }
+            { post.title }
         </p>
         <br/>
         <p class="text-base md:text-xl text-center text-fbksite-darkblue">
@@ -30,7 +43,7 @@
     items-center justify-center w-full h-full">
         <div class="md:w-[60rem] w-[90wh] p-4 overflow-y-visible overflow-x-hidden 
         text-black wrap-anywhere">
-            {@html data.post.content }
+            {@html post.content }
         </div>
     </div>
 </div>
--- src/routes/translations/[slug]/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-import { posts } from '../tld.js';
-
-export function load({ params }) {
-	const post = posts.find((post) => post.slug === params.slug);
-	if (!post) error(404);
-	return { post };
-}
